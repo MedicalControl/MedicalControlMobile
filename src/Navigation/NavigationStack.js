4a5e4399467ccf245234f369c99e32eeef9c5f4e
Melanie Arias <Melaaris19@gmail.com>
2024-04-17 08:49:50 -0600

    Hola

--- src/Navigation/NavigationStack.js
+++ src/Navigation/NavigationStack.js
@@ -14,7 +14,7 @@ export function MyStack() {
                 component={Login}
                 options={{
                     headerShown: false
-                }}
+                }}   
             />
             <HomeStackNa.Screen
                 name='Register'
