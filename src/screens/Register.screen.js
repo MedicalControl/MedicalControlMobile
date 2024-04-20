8316c5777862cb2ec7bd707ff7a5ab9f47cbd940
Melanie Arias <Melaaris19@gmail.com>
2024-04-19 21:44:15 -0600

    DateTime

--- src/screens/Register.screen.js
+++ src/screens/Register.screen.js
@@ -2,15 +2,17 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useForm } from 'react-hook-form'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import RNPickerSelect from 'react-native-picker-select';
-import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import DatePicker from "react-native-date-picker";
 
 //Componentes
 import { Inputs } from "../components/index";
 //constants
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from "../constants/Screen";
+import { Button } from "react-native";
+
 
 export const Register = () => {
     const back = useNavigation();
@@ -32,11 +34,9 @@ export const Register = () => {
         })
     }
 
-    //Municipios
-    const selectedItem = {
-        title: 'Selected item title',
-        description: 'Secondary long descriptive text ...',
-    };
+    //creando calendario
+    const [date, setDate] = useState(new Date())
+    const [open, setOpen] = useState(false)
 
     return (
         <View style={style.container}>
@@ -109,30 +109,27 @@ export const Register = () => {
                             width: SCREEN_WIDTH * 0.5,
                             paddingVertical: 10
                         }} />
-
                     <RNPickerSelect
-                   
                         style={{
                             inputAndroid: {
                                 backgroundColor: '#D8D9ED',
                             },
                         }}
                         onValueChange={(value) => console.log(value)}
-                        placeholder= 'Municipio'
                         items={[
-                            { label: 'Masaya', value: 'masaya' },
-                            { label: 'Leon', value: 'leon' },
-                            { label: 'Granada', value: 'granada' },
-                            { label: 'Esteli', value: 'esteli' },
-                            { label: 'Managua', value: 'managua' },
-                            { label: 'Diriamba', value: 'diriamba' },
-                            { label: 'Carazo', value: 'carazo' },
-                            { label: 'Matagalpa', value: 'matagalpa' },
-                            { label: 'Jinotega', value: 'jinotega' },
-                            { label: 'Rivas', value: 'rivas' },
-                            { label: 'Ticuantepe', value: 'ticuantepe' },
-                            { label: 'Tipitapa', value: 'tipitapa' },
-                            { label: 'Ciudad Sandino', value: 'ciudad sandino' }
+                            { label: 'Masaya', value: 1 },
+                            { label: 'Leon', value: 2 },
+                            { label: 'Granada', value: 3 },
+                            { label: 'Esteli', value: 4 },
+                            { label: 'Managua', value: 5 },
+                            { label: 'Diriamba', value: 6 },
+                            { label: 'Carazo', value: 7 },
+                            { label: 'Matagalpa', value: 8 },
+                            { label: 'Jinotega', value: 9 },
+                            { label: 'Rivas', value: 10 },
+                            { label: 'Ticuantepe', value: 11 },
+                            { label: 'Tipitapa', value: 12 },
+                            { label: 'Ciudad Sandino', value: 13 }
                         ]}
                     />
                     <Inputs
@@ -160,12 +157,15 @@ export const Register = () => {
                         }}
                         onValueChange={(value) => console.log(value)}
                         items={[
-                            { label: 'Estudiante', value: 'estudiante' },
-                            { label: 'Ingeniero', value: 'ingeniero' },
-                            { label: 'Medico', value: 'medico' },
-                            { label: 'Contador', value: 'contador' }
+                            { label: 'Estudiante', value: 123 },
+                            { label: 'Ingeniero', value: 456 },
+                            { label: 'Medico', value: 789 },
+                            { label: 'Contador', value: 498 }
                         ]}
-                        placeholder='Municipio'
+                    />
+                    <Button title="Open" onPress={() => setOpen(true)} />
+                    <DatePicker
+                        mode="date"
                     />
                 </View>
                 <TouchableOpacity onPress={handleSubmit(Onsubmit)}>
