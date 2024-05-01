589d0acc6a4e465a427faa024e31d993820c3e04
Melanie Arias <Melaaris19@gmail.com>
2024-05-01 15:30:28 -0600

    Navigation

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,21 +2,19 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-export function Button() {
-    const Navigation = useNavigation();
-    return (
-        <TouchableOpacity
-            onPress={() => Navigation.navigate('Register')}>
-            <Text style={styles.Sing_Up}>Registare</Text>
-        </TouchableOpacity>
-    )
+const Navigation = useNavigation();
+export const Button = ({ destination }) => {
+    <TouchableOpacity
+        onPress={() => Navigation.navigate(destination)}>
+        <Text style={styles.Sing_Up}>Registare</Text>
+    </TouchableOpacity>
 }
 const styles = StyleSheet.create({
     Sing_Up: {
         fontSize: 15,
         color: "#2AB9B7",
         textDecorationLine: 'underline',
-        textDecorationColor: '#2AB9B7', 
+        textDecorationColor: '#2AB9B7',
         left: 5
     }
 })
