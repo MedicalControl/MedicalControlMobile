0ed3c8f1cb5e73ba0ec1a686cb58c221cc7ec44b
Elex03 <eacuna042@gmail.com>
2024-05-05 20:12:56 -0600

    test image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import Navigation from "./Navigation/index";
+import React, { useEffect, useState } from "react";
+import { View, Image, StyleSheet } from "react-native";
 
 export default function App() {
-  return ( <Navigation/> )
+  const [imageURI, setImageURI] = useState("");
+
+  useEffect(() => {
+    fetch("https://medicalcontrolapi.onrender.com/api/getImages/")
+      .then((response) => {
+        if (!response.ok) throw new Error("Cannot get the uri");
+        return response.json();
+      })
+      .then((data) => {
+        const imageName = data; 
+        console.log(imageName);
+        const uri = `https://medicalcontrolapi.onrender.com/1-mokeywit.png`;
+        setImageURI(uri);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {imageURI !== "" && (
+        <Image source={{ uri: imageURI }} style={styles.image} />
+      )}
+    </View>
+  );
 }
 
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
